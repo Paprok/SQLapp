0d709b14501b3e636b07654df91e5fff61cebc8f
Paprok <rafal.becla@gmail.com>
2018-12-28 20:41:06 +0100

    runs app with termial view

--- src/main/java/com/codecool/app/App.java
+++ src/main/java/com/codecool/app/App.java
@@ -1,10 +1,15 @@
 package com.codecool.app;
 
 
+import com.codecool.app.views.TerminalView;
+import com.codecool.app.views.View;
+
 public class App
 {
     public static void main( String[] args )
     {
-        
+        View terminalView = new TerminalView();
+        TerminalApp app = new TerminalApp(terminalView);
+        app.run();
     }
 }
