84eda83579dc9ad7bab89fe895e779ef049b74d8
Paprok <rafal.becla@gmail.com>
2018-12-29 20:27:05 +0100

    created controller

--- src/main/java/com/codecool/app/controllers/Controller.java
+++ src/main/java/com/codecool/app/controllers/Controller.java
@@ -1,5 +1,21 @@
 package com.codecool.app.controllers;
 
-public class Controller {
+import com.codecool.app.dao.DAOSimple;
+import com.codecool.app.views.View;
 
+import java.util.List;
+
+public class Controller {
+    private DAOSimple daoSimple;
+    private View view;
+
+    public Controller(DAOSimple daoSimple, View view) {
+        this.daoSimple = daoSimple;
+        this.view = view;
+    }
+
+    public void printMentorNames(){
+        List<List<String>> names = daoSimple.getMentorNames();
+        view.printResults(names);
+    }
 }
