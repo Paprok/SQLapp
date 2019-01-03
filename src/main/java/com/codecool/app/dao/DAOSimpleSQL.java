3c5c681cdda5ad7844a11b2ef437a14fee4b7b92
Paprok <rafal.becla@gmail.com>
2019-01-03 18:42:00 +0100

    fixed converting result set to list

--- src/main/java/com/codecool/app/dao/DAOSimpleSQL.java
+++ src/main/java/com/codecool/app/dao/DAOSimpleSQL.java
@@ -1,9 +1,6 @@
 package com.codecool.app.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -17,20 +14,33 @@ public class DAOSimpleSQL implements DAOSimple {
 
     @Override
     public List<List<String>> getMentorNames() throws NoSuchElementException {
-        String sql = "";
+        String sql = "SELECT first_name, last_name FROM mentors";
         ResultSet resultSet;
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             resultSet = ps.executeQuery();
+            return convertResultSetToList(resultSet);
         } catch (SQLException e){
             throw new NoSuchElementException("No names found");
         }
-        return convertResultSetToList(resultSet);
     }
 
-    private List<List<String>> convertResultSetToList(ResultSet resultSet){
+    private List<List<String>> convertResultSetToList(ResultSet resultSet) throws SQLException{
         List<List<String>> list = new ArrayList<>();
-        //TODO
+        List<String> line;
+        while(resultSet.next()) {
+            line = new ArrayList<>();
+            for(int i =1; i <= columnNumber(resultSet); i++) {
+                String result = resultSet.getString(i);
+                line.add(result);
+            }
+            list.add(line);
+        }
         return list;
+    }
+
+    private int columnNumber(ResultSet resultSet) throws SQLException{
+        ResultSetMetaData data = resultSet.getMetaData();
+        return  data.getColumnCount();
     }
 }
