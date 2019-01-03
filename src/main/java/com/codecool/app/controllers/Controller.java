8b7895bae9ae913fcb61c6865d3c06ed41d8d21e
Paprok <rafal.becla@gmail.com>
2019-01-03 19:16:20 +0100

    can print miscolc nicks now

--- src/main/java/com/codecool/app/controllers/Controller.java
+++ src/main/java/com/codecool/app/controllers/Controller.java
@@ -4,6 +4,7 @@ import com.codecool.app.dao.DAOSimple;
 import com.codecool.app.views.View;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Controller {
     private DAOSimple daoSimple;
@@ -14,8 +15,21 @@ public class Controller {
         this.view = view;
     }
 
-    public void printMentorNames(){
-        List<List<String>> names = daoSimple.getMentorNames();
-        view.printResults(names);
+    public void printMentorNames() {
+        try {
+            List<List<String>> names = daoSimple.getMentorNames();
+            view.printResults(names);
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void printMiscolcsNicks() {
+        try {
+            List<List<String>> nicks = daoSimple.getMiscolcNicks();
+            view.printResults(nicks);
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
